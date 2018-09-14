7b8524ba439840e6cf5ebddb80804e6a2c848edb
zhuming <15735929578@163.com>
2018-09-14 17:56:28 +0800

    springboot框架提交

--- src/main/java/com/akera/Application.java
+++ src/main/java/com/akera/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    //这是master分支
+    //这是dev分支
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
