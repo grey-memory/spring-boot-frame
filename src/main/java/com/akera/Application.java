cec2b1c7526959b684fea78395db144579c5ed1d
zhuming <15735929578@163.com>
2018-09-18 14:16:48 +0800

    no message

--- src/main/java/com/akera/Application.java
+++ src/main/java/com/akera/Application.java
@@ -2,7 +2,9 @@ package com.akera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * springboot 启动类
+ * */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
